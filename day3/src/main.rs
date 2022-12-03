9ed341bcc017ed4f0f8c3e5036ac6479eb28fdde
The0x539 <the0x539@gmail.com>
2022-12-02 23:14:04 -0600

    slightly cleaner day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -10,6 +10,10 @@ fn parse(s: &'static str) -> In {
     s
 }
 
+fn set(s: &str) -> HashSet<char> {
+    s.chars().collect()
+}
+
 fn priority(c: char) -> u32 {
     match c {
         'a'..='z' => (c as u32 - 'a' as u32) + 1,
@@ -22,10 +26,8 @@ fn part1(n: &[In]) -> Out {
     let mut sum = 0;
     for s in n {
         let (a, b) = s.split_at(s.len() / 2);
-        let aa = a.chars().collect::<HashSet<_>>();
-        let bb = b.chars().collect::<HashSet<_>>();
-        let c = aa.intersection(&bb).next().unwrap();
-        sum += priority(*c);
+        let ab = &set(a) & &set(b);
+        sum += priority(ab.into_iter().next().unwrap());
     }
     sum
 }
@@ -33,12 +35,9 @@ fn part1(n: &[In]) -> Out {
 fn part2(n: &[In]) -> Out {
     let mut sum = 0;
     for [a, b, c] in n.array_chunks() {
-        let aa = a.chars().collect::<HashSet<_>>();
-        let bb = b.chars().collect::<HashSet<_>>();
-        let cc = c.chars().collect::<HashSet<_>>();
-        let ab = aa.intersection(&bb).copied().collect::<HashSet<_>>();
-        let abc = ab.intersection(&cc).next().unwrap();
-        sum += priority(*abc);
+        let ab = &set(a) & &set(b);
+        let abc = &ab & &set(c);
+        sum += priority(abc.into_iter().next().unwrap());
     }
     sum
 }
