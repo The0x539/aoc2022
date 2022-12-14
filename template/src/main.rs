ac3aa0fde48e80d58f3ff58abc43dcb5839f6605
The0x539 <the0x539@gmail.com>
2022-12-13 23:24:27 -0600

    update template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,20 @@
 #![cfg_attr(test, feature(test))]
 
+use util::poarse as p;
+
 type In = i32;
-type Out = i64;
+type Out = usize;
 
 fn parse(s: &'static str) -> In {
-    util::poarse(s)
+    p(s)
 }
 
 fn part1(n: &[In]) -> Out {
-    n.iter().sum::<i32>() as i64
+    Default::default()
 }
 
 fn part2(n: &[In]) -> Out {
-    n.iter().product::<i32>() as i64
+    Default::default()
 }
 
 util::register!(parse, part1, part2);
