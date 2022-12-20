123cddf82e3146a7f799de8886b2c2d2afcb75f0
The0x539 <the0x539@gmail.com>
2022-12-20 00:25:48 -0600

    busted, but workable, 20p2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -118,15 +118,86 @@ fn mix_part2(n: &mut [Num]) {
     }
 }
 
+fn mix_part1_redux(real_n: &mut [N]) {
+    let mut n = real_n
+        .iter()
+        .copied()
+        .enumerate()
+        .map(Num::new)
+        .collect::<Vec<_>>();
+
+    let mut locations = (0..n.len()).collect::<Vec<_>>();
+    mix_redux(&mut n, &mut locations);
+
+    for i in 0..n.len() {
+        real_n[i] = n[i].value;
+    }
+}
+
+fn mix_redux(n: &mut [Num], locations: &mut [usize]) {
+    let nl = n.len() as N;
+
+    for ii in 0..n.len() {
+        let i = locations[ii];
+        let v = &mut n[i];
+        //assert_eq!(v.index, ii);
+
+        let mut j = i as N + v.value;
+        // ??????
+        if j > nl {
+            j += 1;
+        }
+        j %= nl;
+        if j <= 0 {
+            j += nl - 1;
+        }
+
+        let j = j as usize;
+
+        // println!("moved {} to {}", v.value, j);
+
+        if j == i {
+            // for x in &*n {
+            //     print!("{} ", x.value);
+            // }
+            // println!();
+
+            continue;
+        }
+
+        if j > i {
+            n[i..=j].rotate_left(1);
+            for k in i..j {
+                let kk = n[k].index;
+                locations[kk] -= 1;
+            }
+            locations[ii] = j;
+        } else {
+            // move v to behind us
+            n[j..=i].rotate_right(1);
+            for k in (j + 1)..=i {
+                let kk = n[k].index;
+                locations[kk] += 1;
+            }
+            locations[ii] = j;
+        }
+
+        // for x in &*n {
+        //     print!("{} ", x.value);
+        // }
+        // println!();
+    }
+}
+
 fn part1(n: &[In]) -> Out {
     let mut n = n.to_vec();
-    mix(&mut n);
+    mix_part1_redux(&mut n);
 
     let i0 = n.iter().position(|x| *x == 0).unwrap();
     [1000, 2000, 3000]
         .map(|i| n[(i0 + i) % n.len()])
         .iter()
-        .sum()
+        .sum::<N>()
 }
 
 fn part2(n: &[In]) -> Out {
@@ -145,9 +216,15 @@ fn part2(n: &[In]) -> Out {
         .map(Num::new)
         .collect::<Vec<_>>();
 
+    let mut locations = (0..n.len()).collect::<Vec<_>>();
+
     for _ in 0..10 {
-        println!("{n:?}");
-        mix_part2(&mut n);
+        mix_redux(&mut n, &mut locations);
+
+        for x in &n {
+            print!("{} ", x.value);
+        }
+        println!();
     }
 
     let i0 = n.iter().position(|x| x.value == 0).unwrap();
