a0a7dc4ea8efdf89b783b5194d33d7041ca61406
The0x539 <the0x539@gmail.com>
2022-12-20 00:44:53 -0600

    good day 20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -15,222 +15,82 @@ fn parse(s: &'static str) -> In {
 struct Num {
     value: N,
     index: usize,
-    processed: bool,
 }
 
 impl Num {
     pub fn new((index, value): (usize, N)) -> Self {
-        Self {
-            value,
-            index,
-            processed: false,
-        }
+        Self { value, index }
     }
 }
 
-fn mix(real_n: &mut [N]) {
-    let mut n = real_n
-        .iter()
-        .copied()
-        .enumerate()
-        .map(Num::new)
-        .collect::<Vec<_>>();
-
-    let nl = n.len() as N;
-
-    let mut i = 0;
-    while i < n.len() {
-        let v = &mut n[i];
-
-        if v.processed {
-            i += 1;
-            continue;
-        }
-
-        v.processed = true;
-
-        let mut j = i as N + v.value;
-        while j <= 0 {
-            j += nl - 1;
-        }
-        while j >= nl {
-            j -= nl - 1;
-        }
-
-        let j = j as usize;
-        if j == i {
-            i += 1;
-            continue;
-        }
-
-        if j > i {
-            n[i..=j].rotate_left(1);
-        } else {
-            // move v to behind us
-            n[j..=i].rotate_right(1);
-            i += 1;
-        }
-    }
-
-    for i in 0..n.len() {
-        real_n[i] = n[i].value;
-    }
+fn prepare(n: &[N]) -> (Vec<Num>, Vec<usize>) {
+    let nums = n.iter().copied().enumerate().map(Num::new).collect();
+    let locations = (0..n.len()).collect();
+    (nums, locations)
 }
 
-fn mix_part2(n: &mut [Num]) {
-    let nl = n.len() as N;
+fn mix(nums: &mut [Num], locations: &mut [usize]) {
+    let nl = nums.len() as N;
 
-    for ii in 0..n.len() {
-        let i = n.iter().position(|v| v.index == ii).unwrap();
-        let v = &mut n[i];
-
-        if v.processed {
-            continue;
-        }
-
-        v.processed = true;
-
-        let mut j = i as N + v.value;
-        while j <= 0 {
-            j += nl - 1;
-        }
-        while j >= nl {
-            j -= nl - 1;
-        }
-
-        let j = j as usize;
-        if j == i {
-            continue;
-        }
-
-        if j > i {
-            n[i..=j].rotate_left(1);
-        } else {
-            // move v to behind us
-            n[j..=i].rotate_right(1);
-        }
-
-        println!("{ii}");
-    }
-
-    for x in n {
-        x.processed = false;
-    }
-}
-
-fn mix_part1_redux(real_n: &mut [N]) {
-    let mut n = real_n
-        .iter()
-        .copied()
-        .enumerate()
-        .map(Num::new)
-        .collect::<Vec<_>>();
-
-    let mut locations = (0..n.len()).collect::<Vec<_>>();
-    mix_redux(&mut n, &mut locations);
-
-    for i in 0..n.len() {
-        real_n[i] = n[i].value;
-    }
-}
-
-fn mix_redux(n: &mut [Num], locations: &mut [usize]) {
-    let nl = n.len() as N;
-
-    for ii in 0..n.len() {
+    for ii in 0..nums.len() {
         let i = locations[ii];
-        let v = &mut n[i];
-        //assert_eq!(v.index, ii);
+        let v = &mut nums[i];
+        assert_eq!(ii, v.index);
 
         let mut j = i as N + v.value;
-        // ??????
-        if j > nl {
-            j += 1;
-        }
-        j %= nl;
+        j %= nl - 1;
         if j <= 0 {
             j += nl - 1;
         }
-
         let j = j as usize;
 
-        // println!("moved {} to {}", v.value, j);
-
-        if j == i {
-            // for x in &*n {
-            //     print!("{} ", x.value);
-            // }
-            // println!();
-
-            continue;
-        }
-
         if j > i {
-            n[i..=j].rotate_left(1);
+            nums[i..=j].rotate_left(1);
             for k in i..j {
-                let kk = n[k].index;
+                let kk = nums[k].index;
                 locations[kk] -= 1;
             }
             locations[ii] = j;
-        } else {
-            // move v to behind us
-            n[j..=i].rotate_right(1);
+        } else if j < i {
+            nums[j..=i].rotate_right(1);
             for k in (j + 1)..=i {
-                let kk = n[k].index;
+                let kk = nums[k].index;
                 locations[kk] += 1;
             }
             locations[ii] = j;
         }
-
-        // for x in &*n {
-        //     print!("{} ", x.value);
-        // }
-        // println!();
     }
 }
 
 fn part1(n: &[In]) -> Out {
-    let mut n = n.to_vec();
-    mix_part1_redux(&mut n);
-
-    let i0 = n.iter().position(|x| *x == 0).unwrap();
-    [1000, 2000, 3000]
-        .map(|i| n[(i0 + i) % n.len()])
-        .iter()
-        .sum::<N>()
+    let (mut nums, mut locations) = prepare(n);
+    mix(&mut nums, &mut locations);
+    result(&nums)
 }
 
 fn part2(n: &[In]) -> Out {
-    let mut n = n.to_vec();
+    let (mut nums, mut locations) = prepare(n);
 
     const KEY: N = 811589153;
-
-    for v in &mut n {
-        *v *= KEY;
+    for v in &mut nums {
+        v.value *= KEY;
     }
-
-    let mut n = n
-        .iter()
-        .copied()
-        .enumerate()
-        .map(Num::new)
-        .collect::<Vec<_>>();
-
-    let mut locations = (0..n.len()).collect::<Vec<_>>();
 
     for _ in 0..10 {
-        mix_redux(&mut n, &mut locations);
-
-        for x in &n {
-            print!("{} ", x.value);
-        }
-        println!();
+        mix(&mut nums, &mut locations);
     }
 
-    let i0 = n.iter().position(|x| x.value == 0).unwrap();
-    [1000, 2000, 3000]
-        .map(|i| dbg!(n[(i0 + i) % n.len()].value))
-        .iter()
+    result(&nums)
+}
+
+fn result(n: &[Num]) -> Out {
+    std::iter::repeat(n)
+        .flatten()
+        .map(|v| v.value)
+        .skip_while(|v| *v != 0)
+        .step_by(1000)
+        .skip(1)
+        .take(3)
         .sum()
 }
 
